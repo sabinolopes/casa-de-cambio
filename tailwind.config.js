89e53a1b96bae6ffcedf6f487f421c3289cf4395
Sabino Lopes <sabinolopes007@gmail.com>
2023-08-22 15:12:51 -0300

    first structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
       colors: {
         'bg-primary': '#282C33',
         'tx-second': '#95EBA8',
+        'btn-color': '#96EB95',
+        'inp-bg-color': '#D9D9D91A',
+        'inp-text': '#D9D9D91A',
+        'bg-second': '#FFFFFF0F',
       },
+      fontFamily: {
+        'mono': ['"Fira Code"', 'ui-monospace', 'SFMono-Regular'],
+      },
+      borderRadius: {
+        'lg': '10px',
+        '3xl': '20px',
+      },
+      width: {
+        '44': '171px'
+      }
     },
   },
   plugins: [],
